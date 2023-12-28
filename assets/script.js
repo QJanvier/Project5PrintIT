1b197c4b0be7e3252d534937db8cdf9de2b227de
Quentin Janvier <quentin.janvier@hotmail.fr>
2023-12-28 23:53:26 +0100

    dots added, function pin & slide(WIP)

--- assets/script.js
+++ assets/script.js
@@ -17,14 +17,62 @@ const slides = [
 	}
 ]
 
-
+// move left
 document.getElementById("arrow_left")
-	.addEventListener("click", (event)=>{
-		console.log(event);
+	.addEventListener("click", function() {
+		if (i = 0) {
+			i = slidesLength - 1;
+		}
+		else {
+			i--;
+		}
+		console.log(arrow_left);
+		slideImage();
 	});
 
+// move right
 document.getElementById("arrow_right")
     .addEventListener("click", function () {
-    console.log("turn right")}
-);
+		if (i == slidesLength - 1) {
+			i = 0;
+		}
+		else {
+			i++;
+		}
+    console.log(arrow_right)
+	slideImage();
+	});
 
+const slidesLength = slides.length;
+let i = 0;
+function dots() {
+	const dots = document.querySelector('.dots');
+	for (let i = 0; i < slidesLength; i++) {
+	const dot = document.createElement('span');
+	dot.classList.add('dot');
+	dots.appendChild(dot);
+
+	if (i == 0) {
+		dots.children[i].classList.add('dot_selected')
+	}
+	}
+}
+dots();
+
+/*pinpoint the selected dot */
+function pinPoint() {
+	const dot = document.getElementsByClassName('dot');
+	for (let i = 0; i < dot.length; i++) {
+		dot[i].classList.remove('dot_selected');
+	}
+	dot[i].classList.add('dot_selected');
+
+}
+
+const imgElmt = document.querySelector('#banner > img')
+const elmtName = document.querySelector('#banner > p')
+function slideImage() {
+	imgElmt.src = './assets/images/slideshow/'; //select next img needed in the path
+	elmtName.innerHTML = slides[i].tagLine; // slides[i] undefined when using left arrow
+	pinPoint();
+}
